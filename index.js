c1dd97bf8caf62ca5446ccd39a12e8b75d7749e5
alaydin <burakalaydin@gmail.com>
2022-11-01 19:16:24 +0300

    .

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
 
 app.get('/nativeBalance', async (req, res) => {
